75e733dabcacceb8445c85405ba007dc300c9ece
Nat <ntaktach@gmail.com>
2021-04-20 13:48:38 -0400

    commit tree 805846c54b58ce9d855163a90e58a16ffea47306

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Your code here
+class Polygon {
+    constructor(sides){
+        this.sides = sides
+        this.count = this.sides.length
+    }
+    get countSidse() {
+        return this.sides.length
+    }
+
+    get perimeter () { 
+    }
+}
